bb6fd3e95b3813356473f91b7534bbe6097d75c4
bvsvntv <developer.basanta@gmail.com>
2026-07-22 16:00:51 +0545

    feat(dvdrental-api): load environment variables from .env files

--- system-design/dvdrental-api/go.mod
+++ system-design/dvdrental-api/go.mod
@@ -2,4 +2,7 @@ module dvdrental-api
 
 go 1.25.5
 
-require github.com/go-chi/chi/v5 v5.3.1 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.3.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- system-design/dvdrental-api/main.go
+++ system-design/dvdrental-api/main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("no .env file found, relying on system env vars")
+	}
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World!"))
 	})
-	http.ListenAndServe(":13000", r)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	server := &http.Server{
+		Handler: r,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Server listening at http://localhost:%v", port)
+
+	srvErr := server.ListenAndServe()
+	if srvErr != nil {
+		log.Fatal(srvErr)
+	}
 }
