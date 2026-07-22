11d375d7c310ce6a8e8b8efd6be2e45d8a0da059
bvsvntv <developer.basanta@gmail.com>
2026-07-22 20:39:12 +0545

    chore(dvdrental-api): nuke everything for new architecture

--- system-design/dvdrental-api/main.go
+++ system-design/dvdrental-api/main.go
@@ -1,37 +1,7 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"os"
-
-	"dvdrental-api/config"
-	"dvdrental-api/routes"
-
-	"github.com/joho/godotenv"
-)
+import "fmt"
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Println("no .env file found, relying on system env vars")
-	}
-
-	config.ConnectDB()
-	router := routes.SetupRouter()
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	server := &http.Server{
-		Handler: router,
-		Addr:    ":" + port,
-	}
-
-	log.Printf("Server listening at http://localhost:%v", port)
-
-	srvErr := server.ListenAndServe()
-	if srvErr != nil {
-		log.Fatal(srvErr)
-	}
+	fmt.Println("dvdrental-api")
 }
