6b4588ba24cc0358f6b27f6c56143b97f1b29b6e
bvsvntv <developer.basanta@gmail.com>
2026-07-24 16:45:00 +0545

    chore(dvdrental-api): rename env dir to utils

--- system-design/dvdrental-api/cmd/main.go
+++ system-design/dvdrental-api/cmd/main.go
@@ -5,7 +5,7 @@ import (
 	"log/slog"
 	"os"
 
-	"dvdrental-api/internal/env"
+	"dvdrental-api/internal/utils"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
@@ -17,13 +17,13 @@ func main() {
 	slog.SetDefault(logger)
 
 	if err := godotenv.Load(); err != nil {
-		logger.Info("Couldn't load .env file.", "error", err.Error())
+		logger.Info("Couldn't load .utils file.", "error", err.Error())
 	}
 
 	cfg := config{
-		addr: env.GetString("PORT", "8000"),
+		addr: utils.GetString("PORT", "8000"),
 		db: dbConfig{
-			dsn: env.GetString("DB_URL", "postgres://postgres:postgres@localhost:15432/dvdrental?sslmode=disable"),
+			dsn: utils.GetString("DB_URL", "postgres://postgres:postgres@localhost:15432/dvdrental?sslmode=disable"),
 		},
 	}
 
