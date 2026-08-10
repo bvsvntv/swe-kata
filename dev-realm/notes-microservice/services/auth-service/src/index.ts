ec4f3dce77fa91e9dbaa76d72688f20392d5320b
bvsvntv <developer.basanta@gmail.com>
2026-08-10 17:05:43 +0545

    refactor(notes-microservice): move router to v1

--- dev-realm/notes-microservice/services/auth-service/src/index.ts
+++ dev-realm/notes-microservice/services/auth-service/src/index.ts
@@ -1,18 +1,17 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
+
+import v1Routes from './routes/v1';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 18000;
 
-app.get('/heartbeat', (_req: Request, res: Response) => {
-    const hrtime = process.hrtime.bigint();
-    res.status(200).json({ heartbeat: hrtime.toString() });
-});
+app.use('/api/v1', v1Routes);
 
 app.listen(PORT, () => {
-    console.log(`Auth service listening at http://localhost:${PORT}`);
+    console.log(`Auth service listening at http://localhost:${PORT}/api/v1`);
     console.log(`Environment: ${process.env.SERVER_ENV}`);
-    console.log(`Heartbeat: http://localhost:${PORT}/heartbeat`);
+    console.log(`Heartbeat: http://localhost:${PORT}/api/v1/heartbeat`);
 });
