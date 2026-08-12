563574f14286933b8e589bf776cdb12bf1a7dc2f
bvsvntv <developer.basanta@gmail.com>
2026-08-12 17:40:37 +0545

    feat(notes-microservice): wip global error handling middleware

--- dev-realm/notes-microservice/services/auth-service/src/app.ts
+++ dev-realm/notes-microservice/services/auth-service/src/app.ts
@@ -2,12 +2,14 @@ import express from 'express';
 
 // Routes
 import v1Routes from './routes/v1';
+import { errorHandler } from './middlewares/error.middleware';
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(errorHandler);
 
 app.use('/api/v1', v1Routes);
 
---
+++ dev-realm/notes-microservice/services/auth-service/src/middlewares/error.middleware.ts
@@ -0,0 +1,16 @@
+import { AppError } from '@/types';
+import { Request, Response, NextFunction } from 'express';
+
+export function errorHandler(
+    error: AppError,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) {
+    const statusCode = error.statusCode || 500;
+    const message = error.message || 'Internal server error.';
+
+    res.status(statusCode).json({ error: message });
+
+    next();
+}
