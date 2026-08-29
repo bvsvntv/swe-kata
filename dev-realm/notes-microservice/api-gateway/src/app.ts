057a059b7aa77f8af612529a068d20ac204c448b
bvsvntv <developer.basanta@gmail.com>
2026-08-29 22:10:28 +0545

    chore(notes-microservice): add cors & helmet

--- dev-realm/notes-microservice/api-gateway/src/app.ts
+++ dev-realm/notes-microservice/api-gateway/src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 import { rateLimiter } from './middlewares/rateLimiter.middleware';
 import { unknownRouteHandler } from '@shared/src/middlewares/unknownRoute.middleware';
 import { errorHandler } from '@shared/src/middlewares/error.middleware';
@@ -8,6 +10,9 @@ const app = express();
 
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true }));
+
+app.use(helmet());
+app.use(cors());
 app.use(rateLimiter);
 
 app.use(gatewayRoutes);
