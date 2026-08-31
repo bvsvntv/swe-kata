da5b97d20c93689f9867b2a7c4e12f6c128aa841
bvsvntv <developer.basanta@gmail.com>
2026-08-31 17:57:59 +0545

    wip(notes-microservice): wip prometheus setup

--- dev-realm/notes-microservice/api-gateway/src/app.ts
+++ dev-realm/notes-microservice/api-gateway/src/app.ts
@@ -1,12 +1,20 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import client from '@prometheus-io/client';
 import { rateLimiter } from './middlewares/rateLimiter.middleware';
 import { unknownRouteHandler } from '@shared/src/middlewares/unknownRoute.middleware';
 import { errorHandler } from '@shared/src/middlewares/error.middleware';
 import gatewayRoutes from './routes';
 
 const app = express();
+
+client.collectDefaultMetrics();
+
+app.get('/metrics', async (_req, res) => {
+    res.set('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
+});
 
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true }));
