6d1ab7bb3f0c936de9503b8d8606677807bb4de3
bvsvntv <developer.basanta@gmail.com>
2026-09-08 15:24:59 +0545

    feat(elasticsearch-lab): wip search comparison ui

--- dev-realm/elasticsearch-lab/app/page.tsx
+++ dev-realm/elasticsearch-lab/app/page.tsx
@@ -1,17 +1,49 @@
+import { HugeiconsIcon } from "@hugeicons/react"
 import { Button } from "@/components/ui/button"
+import {
+  Empty,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+  EmptyDescription,
+  EmptyContent,
+} from "@/components/ui/empty"
+import { Field } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { SearchAlertIcon } from "@hugeicons/core-free-icons"
 
 export default function Page() {
   return (
     <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
+      <div className="mx-auto flex max-w-7xl min-w-md flex-col gap-4 text-sm leading-loose">
         <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
+          <h1 className="font-medium">
+            Text search in Database V/S Elastisearch
+          </h1>
+
+          <Field className="mt-4" orientation="horizontal">
+            <Input type="search" placeholder="Search..." />
+            <Button>Search</Button>
+          </Field>
         </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
+
+        <div>
+          <Empty>
+            <EmptyHeader>
+              <EmptyMedia variant="icon">
+                <HugeiconsIcon
+                  icon={SearchAlertIcon}
+                  size={24}
+                  color="currentColor"
+                  strokeWidth={1.5}
+                />
+              </EmptyMedia>
+              <EmptyTitle>Waiting for search</EmptyTitle>
+              <EmptyDescription>
+                Enter a search term and click "Search" to see results.
+              </EmptyDescription>
+            </EmptyHeader>
+          </Empty>
         </div>
       </div>
     </div>
